418c381e03e78db6c74501daadc1e6d966de7166
Dipesh Dhote <dpsh.dhote@gmail.com>
2024-03-10 16:50:37 +0530

    add

--- src/crop_recommendation/components/data_ingestion.py
+++ src/crop_recommendation/components/data_ingestion.py
@@ -20,7 +20,7 @@ class DataIngestion:
 
     def initiate_data_ingestion(self):
         try:
-            df=read_sql_data()
+            df=pd.read_csv(os.path.join('notebook/data','raw/csv'))
             logging.info("reading complited from mysql database")
 
             os.makedirs(os.path.dirname(self.ingestionconfig.train_data_path),exist_ok=True)
